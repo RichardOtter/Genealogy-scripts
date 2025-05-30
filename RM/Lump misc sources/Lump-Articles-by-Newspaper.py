55875472ec549e02859c196c0863128b60474857
Richard Otter <RichardJOtter@gmail.com>
2025-05-30 14:57:12 -0700

    Update- not yet working

--- RM/Lump misc sources/Lump-Articles-by-Newspaper.py
+++ RM/Lump misc sources/Lump-Articles-by-Newspaper.py
@@ -1,94 +1,185 @@
-import sqlite3
-import os
-import time
-from pathlib  import Path
-from datetime import datetime
-import configparser
+# OLD TODO
 import xml.etree.ElementTree as ET
+
+
 import sys
+from pathlib import Path
+sys.path.append( str(Path.resolve(Path.cwd() / r'..\RMpy package')))
+import RMpy.launcher          # type: ignore
+import RMpy.common as RMc     # type: ignore
+from RMpy.common import q_str # type: ignore
 
 
-## WARNING make a known-good backup of the rmtree file before use.
+# Requirements:
+#   RootsMagic database file
+#   RM-Python-config.ini
 
-##  Requirements: (see ReadMe.txt for details)
-##   RootsMagic v8 or 9 database file
-##   RM-Python-config.ini  ( Configuration ini file to set options and parameters)
-##   unifuzz64.dll
-##   tested with Python v3.10
-##  Using RM, determine-
-##   * the set of sources to be lumped - here all starting with C1940 and having a state abbreviation
-##   * an existing source that the old sources will be made citations of
-##   * an existing citation named "sample citation" in the new source. (Others will not interfere)
+# Tested with:
+#   RootsMagic database file v10
+#   Python for Windows v3.13
+
+# Config files fields used
+#    FILE_PATHS  REPORT_FILE_PATH
+#    FILE_PATHS  REPORT_FILE_DISPLAY_APP
+#    FILE_PATHS  DB_PATH
+#
+# OLD TODO
+#    MAPPING     FACTTYPE_CURRENT
+#    MAPPING     FACTTYPE_NEW
+#    MAPPING     ROLE
+#    SOURCE_FILTER     DESC
+#    SOURCE_FILTER     DATE
 
 
 G_count = 0
 
 # ================================================================
 def main():
-  global _G_count
 
-  # Configuration file
-  IniFile="RM-Python-config.ini"
+    # Configuration
+    utility_info = {}
+    utility_info["utility_name"]      = "Lump_Newspapers" 
+    utility_info["utility_version"] = "UTILITY_VERSION_NUMBER_RM_UTILS_OVERRIDE"
+    utility_info["config_file_name"]  = "RM-Python-config.ini"
+    utility_info["script_path"]  = Path(__file__).parent
+    utility_info["run_features_function"]  = run_selected_features
+    utility_info["allow_db_changes"]  = True
+    utility_info["RMNOCASE_required"] = True
+    utility_info["RegExp_required"]   = False
 
-  # ini file must be in "current directory" and encoded as UTF-8 if non-ASCII chars present (no BOM)
-  if not os.path.exists(IniFile):
-      print("ERROR: The ini configuration file, " + IniFile + " must be in the current directory." )
-      return
+    RMpy.launcher.launcher(utility_info)
 
 
-  config = configparser.ConfigParser()
-  config.read(IniFile, 'UTF-8')
+# ===================================================DIV60==
+def run_selected_features(config, db_connection, report_file):
 
-  # Read file paths from ini file
-  database_Path = config['File Paths']['DB_PATH']
-  RMNOCASE_Path = config['File Paths']['RMNOCASE_PATH']
+    lump_citations(config, db_connection, report_file)
 
-  if not os.path.exists(database_Path):
-      print('Database path not found. Fix configuration file and try again.')
-      return
 
-  # Process the database
-  with create_DBconnection(database_Path) as conn:
-    conn.enable_load_extension(True)
-    conn.load_extension(RMNOCASE_Path)
+## global _G_count
+
+# ===================================================DIV60==
+def lump_citations(config, db_connection, report_file):
 
   # Deal with index and the fact that the collation sequence is different here than in RM
   SqlStmt_ReIndex= """
    REINDEX RMNOCASE;
    """
 
-  cur = conn.cursor()
+  cur = db_connection.cursor()
   cur.execute(SqlStmt_ReIndex)
 
+  old_src_TemplateID= 10070
+  new_src_TemplateID= 10071
 
-# The list of states to lump in this run
-#   new SourceID, state abbrev  1850
-  stateList = [
-     (6462, "LA"),
-     (6461, "IN") ]
+  lumped_source_list = [
+#    (6813, "-pTBE", "NP US,NY- Brooklyn Eagle")
+#    (6814, "-pHSA", "NP US,HI- Honolulu Star-Advertiser")
+    (6815, "%-pHTH=%", "NP US,HI- Hawaii Tribune-Herald")
+#    (6816, "-pNDNE", "NP US,NY- Newsday (Nassau Edition) ")
+#    (6817, "-pHSB", "NP US,HI- Honolulu Star-Bulletin")
+#    (6818, "NP US,IN- Palladium-Item")
+#    (6819, "NP US,NJ- The Daily Register")
+#    (6820, "NP US,NY- Kings County Rural Gazette")
+#    (6821, "NP US,CA- Daily Independent Journal")
+#    (6822, "NP US,IN- The Star Press")
+#    (6823, "NP US,FL- The Naples Daily News")
+#    (6824, "NP US,LA- The Times-Picayune")
+#    (6825, "NP US,IN- The News-Examiner")
+#    (6826, "NP US,FL- South Florida Sun-Sentinel")
+#    (6827, "NP US,NY- The Journal News")
+#    (6828, "NP US,CA- Los Angeles Sentinel")
+#    (6829, "NP US,FL- The Banner")
+#    (6830, "NP US,NY- Long Island Daily Press")
+#    (6831, "NP US,CA- The San Francisco Examiner")
+#    (6832, "NP US,NY- New York Post")
+#    (6833, "NP US,MF- The Baltimore Sun")
+#    (6834, "NP US,OH- The Zanesville Signal")
+#    (6835, "NP US,OH- The Piqua Daily Call")
+#    (6836, "NP US,PA- The Gettysburg Times")
+#    (6837, "NP US,NH- Nashua Telegraph")
+#    (6838, "NP US,OH- Mansfield News Journal")
+#    (6839, "NP US,OH- The Daily Advocate")
+#    (6840, "NP US,OH- Miami Union")
+#    (6841, "NP US,OH- Troy Daily News")
+#    (6842, "NP US,KY- The Kentucky Enquirer")
+#    (6843, "NP US,KY- Lexington Herald-Leader")
+#    (6844, "NP US,OH- The Arcanum Times")
+#    (6845, "NP US,OH- News Journal")
+#    (6847, "NP US,OH- Dayton Daily News")
+#    (6848, "NP US,OH- The Dayton Herald")
+#    (6849, "NP US,TN- Evening Herald Courier")
+#    (6850, "NP US,IN- The Journal-Press")
+#    (6851, "NP US,IN- The Aurora Journal")
+#    (6852, "NP US,OH- The Galion Inquirer")
+#    (6853, "NP US,IN- The Indianapolis News")
+#    (6854, "NP US,OH- The Journal News")
+#    (6855, "NP US,CA- The Sacramento Bee")
+#    (6856, "NP US,NJ- Asbury Park Press")
+#    (6857, "NP US,NJ- The Ocean Star")
+#    (6860, "NP US,WI- The Daily Tribune")
+#    (6861, "NP US,FL- News-Press")
+#    (6862, "NP US,NY- The Post-Standard")
+#    (6863, "NP US,CA- The Los Angeles Times")
+#    (6864, "NP US,IN- The Call-Leader")
+#    (6865, "NP US,CA- The Desert Sun")
+#    (6866, "NP US,OH- Springfield News-Sun")
+#    (6867, "NP US,IN- Tipton County Tribune")
+#    (6868, "NP US,AZ- Arizona Republic")
+#    (6869, "NP US,OR- Statesman Journal")
+#    (6870, "NP US,IN- Muncie Evening Press")
+#    (6871, "NP US,FL- The Orlando Sentinel")
+#    (6872, "NP US,FL- The Bradenton Herald")
+#    (6873, "NP US,PA- The Morning Call")
+#    (6875, "NP US,HI- West Hawaii Today")
+#    (6877, "NP US,GA- The Atlanta Constitution")
+#    (6878, "NP US,IN- The Daily Reporter")
+#    (6879, "NP US,IN- The Commercial-Mail")
+#    (6880, "NP US,IN- The Kokomo Tribune")
+#    (6881, "NP US,NY- The Ithaca Journal")
+#    (6882, "NP US,FL- Pensacola News Journal")
+#    (6883, "NP US,MA- The Boston Globe")
+#    (6884, "NP US,NY- Times Union")
+#    (6885, "NP US,OH- The Circleville Herald")
+#    (6886, "NP US,NY- Daily News")
+#    (6887, "NP US,NJ- The Herald-News")
+#    (6888, "NP US,NY- The Standard Union")
+#    (6889, "NP US,FL- The Miami Herald")
+#    (6890, "NP US,MI- Detroit Free Press")
+#    (6892, "NP US,NV- Reno Gazette-Journal")
+#    (6893, "NP US,TX- Corpus Christi Times")
+#    (6894, "NP US,IN- The Brookville Democrat")
+#    (6895, "NP US,CT- Hartford Courant")
+#    (6896, "NP US,IN- The Terre Haute Tribune")
+#    (6897, "NP US,IL- Pekin Daily Times ")
+    ]
+    
 
-
-  for state in stateList:
+  for source in lumped_source_list:
 
     # specify the single source that is to get the new citations
-    NewSourceID = state[0]
-    StateAbbrev=  state[1]
+    NewSourceID = source[0]
+    src_name_identifier=  source[1]
    
     # List the sources which will be converted to citations of the 'new' source
-    Part1 = " SELECT SourceID FROM SourceTable WHERE  Name LIKE 'C1850-"
-    Part2 = " %' AND TemplateID=10026"
-    # NOTE this sql contains the old SourceTemplateID and depends on format of source names to be processed
+    SQL_stmt = "SELECT SourceID FROM SourceTable WHERE Name LIKE ? AND TemplateID=?"
+  #  SourcesToLump = GetListOfRows( db_connection, SQL_stmt, (src_name_identifier, old_src_TemplateID))
    
-    SqlStmt_OldCensus = Part1 + StateAbbrev + Part2
-   
-    SourcesToLump = GetListOfRows( conn, SqlStmt_OldCensus)
-   
+    cur = db_connection.cursor()
+    subs=(src_name_identifier, old_src_TemplateID)
+    cur.execute(SQL_stmt, subs)
+
+    SourcesToLump = []
+    for t in cur:
+      for x in t:
+        SourcesToLump.append(x)
+
     print ("\n\n\n number of source to process: ", len(SourcesToLump))
    
     #iterate through each of the old sources, converting each one separately
     for oldSrc in SourcesToLump:
         print ("old SourceID= " + str(oldSrc) )
-        ConvertSource (conn, oldSrc, NewSourceID)
+        ConvertSource (db_connection, oldSrc, NewSourceID)
         print ("=====================================================")
 
   print ("total not found",G_count)
@@ -317,17 +408,6 @@ def getCitationsToMove ( conn, oldSourceID):
 
 
 # ================================================================
-def create_DBconnection(db_file):
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
-
-    return conn
-
-
-# ================================================================
 def GetListOfRows ( conn, SqlStmt):
     # SqlStmt should return a set of single values
     cur = conn.cursor()
@@ -340,9 +420,9 @@ def GetListOfRows ( conn, SqlStmt):
     return result
 
 
-# ================================================================
+# ===================================================DIV60==
 # Call the "main" function
 if __name__ == '__main__':
     main()
 
-# ================================================================
+# ===================================================DIV60==
