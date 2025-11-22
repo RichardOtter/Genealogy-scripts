09476fd35c29c06c4c00ec6abdfa014969ba3f8f
Richard Otter <RichardJOtter@gmail.com>
2025-11-21 20:27:58 -0800

    improve set up script

--- dev util scripts/File and folder setup in repo folder.py
+++ dev util scripts/File and folder setup in repo folder.py
@@ -2,31 +2,29 @@ import os
 from pathlib import Path
 import yaml    # type: ignore
 from datetime import datetime
-from pathlib import Path
 
-# As a first step, this build script builds from local source
-# it does not get "fresh files" from GitHub
 
 # this file lives in the dir:  repo root/dev util scripts
 # the top level yaml config file is also here.
 
-
 def main():
 
+    # if a file already exists, delete it
     keep_orig = False
 
     # CONSTANTS
     REPO_ROOT_PATH = Path(
         r"C:\Users\rotter\Development\Genealogy\repo Genealogy-scripts")
-    VERSION_REPLACE_TEXT = r'UTILITY_VERSION_NUMBER_RM_UTILS_OVERRIDE'
     TOP_LEVEL_CONFIG_NAME = r"_top_level_build_config.yaml"
-    RM_PATH = REPO_ROOT_PATH / "RM"
 
     file1="_DB get fresh copy of Production.cmd"
     file2="_DB reset test db.cmd"
     file3="_DB get fresh copy of TestData.cmd"
 
+    doc_folder = REPO_ROOT_PATH / "doc"
+
     try:
+        time_stamp = time_stamp_now("file")
         dev_util_fldr_path= REPO_ROOT_PATH / "dev util scripts"
         top_level_config_path = dev_util_fldr_path / TOP_LEVEL_CONFIG_NAME
         
@@ -43,11 +41,10 @@ def main():
         print("Problem getting the values from the top level yaml file.\n")
         exit()
     
-    doc_folder = REPO_ROOT_PATH / "RM" / "doc"
 
     # Process each project
     for project in project_list:
-        project_dir_path = RM_PATH / project
+        project_dir_path = REPO_ROOT_PATH / project
 
         # create a hard link in the docs folder for each ReaMe
         try:
@@ -62,7 +59,6 @@ def main():
         except FileExistsError:
             print(F"Directory '{project_DB_path}' already exists.")
         # make a hardlink to the db utility cmd files in each DB folder
-
 
         try:
             if not keep_orig:
