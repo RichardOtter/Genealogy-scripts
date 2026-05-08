7a45fd6286ef52ff7ac2df07570fffd99e5d632b
Richard Otter <RichardJOtter@gmail.com>
2026-05-07 21:22:43 -0700

    update for new LU tables

--- Misc SQL/make Otter-Saito RM DB conform to norms/Drop non-RM-standard tables.sql
+++ Misc SQL/make Otter-Saito RM DB conform to norms/Drop non-RM-standard tables.sql
@@ -9,9 +9,15 @@ DROP TABLE AuxMultimediaTable;
 
 -- Look up tables and views
 
-DROP TABLE LU_DNAProviderName;
-DROP TABLE LU_NameType;
 DROP TABLE LU_ColorName;
+DROP TABLE LU_DNAProviderName;
+DROP TABLE LU_FatherLabel.sql;
+DROP TABLE LU_FKeyTarget.sql;
+DROP TABLE LU_MedConditionName.sql;
+DROP TABLE LU_MotherLabel.sql;
+DROP TABLE LU_NameType;
+DROP TABLE LU_ProofName.sql;
+DROP TABLE LU_SexType.sql;
 
 DROP VIEW IF EXISTS LU_ColorCodeSetName;
 DROP VIEW IF EXISTS LU_ColorCodeSetName;
